4b8fce2051b6c66dcb72eaffd4bef194765fe60c
cassis <cripse2000@gmail.com>
2024-10-03 09:25:58 +0200

    Création fichier data.json

--- noSQL/src/router/index.ts
+++ noSQL/src/router/index.ts
@@ -16,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/christel',
+      name: 'christel',
+      component: () => import('../views/ChristelView.vue')
     }
   ]
 })
